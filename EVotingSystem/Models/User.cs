f9fdc81de61a4820f3208977fb17abba5b5a7e06
Milos Petrovic <darthypyro@gmail.com>
2025-12-25 17:38:18 +0100

    created user class and subclasses and can read registration forms for each

---
+++ EVotingSystem/Models/User.cs
@@ -0,0 +1,31 @@
+﻿namespace EVotingSystem.Models
+{
+    // Base class
+    public abstract class User
+    {
+        public string Password { get; set; } // All users have a password
+
+        // You can add common methods later, like VerifyPassword()
+    }
+
+    // Organizer subclass
+    public class Organizer : User
+    {
+        public string OrganizationName { get; set; }
+        public string OrganizationId { get; set; }
+
+        // Organizer-specific methods
+        // public void CreateElection() { ... }
+    }
+
+    // Voter subclass
+    public class Voter : User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+
+        // Voter-specific methods
+        // public void Vote() { ... }
+    }
+}
--- EVotingSystem/Views/RegisterView.xaml.cs
+++ EVotingSystem/Views/RegisterView.xaml.cs
@@ -16,9 +16,6 @@ using System.Windows.Shapes;
 
 namespace EVotingSystem.Views
 {
-    /// <summary>
-    /// Interaction logic for RegisterView.xaml
-    /// </summary>
     public partial class RegisterView : UserControl
     {
         private UserType selectedUserType = UserType.None;
@@ -26,21 +23,21 @@ namespace EVotingSystem.Views
         public RegisterView()
         {
             InitializeComponent();
-            RegisterButton.IsEnabled = false; // disable initially
+            RegisterButton.IsEnabled = false; // Disable until role selected
         }
 
         private void Organizer_Click(object sender, RoutedEventArgs e)
         {
             selectedUserType = UserType.Organizer;
             FormContent.Content = new OrganizerRegisterForm();
-            RegisterButton.IsEnabled = true; // enable now
+            RegisterButton.IsEnabled = true;
         }
 
         private void Voter_Click(object sender, RoutedEventArgs e)
         {
             selectedUserType = UserType.Voter;
             FormContent.Content = new VoterRegisterForm();
-            RegisterButton.IsEnabled = true; // enable now
+            RegisterButton.IsEnabled = true;
         }
 
         private void Back_Click(object sender, RoutedEventArgs e)
@@ -51,10 +48,52 @@ namespace EVotingSystem.Views
 
         private void Register_Click(object sender, RoutedEventArgs e)
         {
-            // Here you can later handle registration using selectedUserType
-            MessageBox.Show($"Registering as {selectedUserType}");
+            User newUser = GetUserFromForm();
+
+            // For now, just show a message to test
+            if (newUser is Organizer organizer)
+            {
+                MessageBox.Show($"Organizer registered: {organizer.OrganizationName}, ID: {organizer.OrganizationId}");
+            }
+            else if (newUser is Voter voter)
+            {
+                MessageBox.Show($"Voter registered: {voter.FirstName} {voter.LastName}, Username: {voter.Username}");
+            }
+
+            // Later: handle certificate generation & storage
+        }
+
+        private User GetUserFromForm()
+        {
+            if (selectedUserType == UserType.Organizer)
+            {
+                var form = FormContent.Content as OrganizerRegisterForm;
+                if (form != null)
+                {
+                    return new Organizer
+                    {
+                        OrganizationName = form.OrganizationNameTextBox.Text,
+                        OrganizationId = form.OrganizationIdTextBox.Text,
+                        Password = form.PasswordBox.Password
+                    };
+                }
+            }
+            else if (selectedUserType == UserType.Voter)
+            {
+                var form = FormContent.Content as VoterRegisterForm;
+                if (form != null)
+                {
+                    return new Voter
+                    {
+                        FirstName = form.FirstNameTextBox.Text,
+                        LastName = form.LastNameTextBox.Text,
+                        Username = form.UsernameTextBox.Text,
+                        Password = form.PasswordBox.Password
+                    };
+                }
+            }
+
+            return null; // should never happen if button is disabled until selection
         }
     }
-
-
 }
